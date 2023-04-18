6d809215cb2a7742490e95e42c0fc08d8ecb6ed4
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-19 00:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //your JS code here. If requir
-const student ={	
-		name : "john", 
-	    age :35, 
-	    city : "new york",
+ class student {	
+	 constructor(name, age, city){
+		this.name = name
+		this.age = age
+	    this.city = city
+	 }
 }
 
 student.prototype.getKeys = function(){
 			return Object.keys(this);
 		} 
 
-const myObject = Object.create(student);
+const myObject = new student("john", 34, "dumka");
 const body = document.querySelector("body");
 body.innerText = myObject.getKeys();
