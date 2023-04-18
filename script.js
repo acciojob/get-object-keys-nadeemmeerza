f1c8f905ceebf569e6d5d8ddfc32a7d1a8c525a5
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-18 23:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+function student(name){
+	return {
+		this.name: name,
+	    this.getKeys:()=>{
+			return Objects.keys(this);
+		} 
+	}
+  
+}
