dddceb7d9fe9666666fe1066aa82a6483ed7cd79
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-18 23:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If requir
-function student(name){
-	
-		this.name: name,
-	    
-	  
+function student(name, age, city){	
+		this.name = name;
+	    this.age = age; 
+	    this.city = city;
 }
 
 student.prototype.getKeys = ()=>{
